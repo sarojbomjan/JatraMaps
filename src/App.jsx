2c2ed671aed053937536068ae2a39658cf7443d1
sahiramaharjan <sahiramaharjan11@gmail.com>
2025-03-22 20:38:50 +0545

    Calendar page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Home from './pages/home'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import CalendarPage from "./pages/calendar";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
