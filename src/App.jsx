98429603b099fbbdaa16c9173564b275724a6ec7
.android <sarojbomjan7@gmail.com>
2025-03-23 07:32:12 +0545

    merged with login

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
-import { Routes , Route } from 'react-router-dom'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const Home = lazy(() => import('./pages/Home'));
+const AboutUs = lazy(() => import('./pages/about'));
+const EventPage = lazy(() => import('./pages/event'));
+const Login = lazy(() => import('./pages/Login'));
+const SignUp = lazy(() => import('./pages/SignUp'));
 
 const App = () => {
   return (
-    <Router>
+  
       <div className="flex flex-col min-h-screen">
-        {/* Navbar*/}
+        {/* Navbar */}
         <Navbar />
 
-      {/* Routings */}
+        {/* Routes */}
         <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<AboutUs/>} />
-            <Route path="/events" element={<EventPage />} />
-            <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-          </Routes>
+          <Suspense fallback={<div className="text-center">Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/events" element={<EventPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </Suspense>
         </div>
 
         {/* Footer */}
         <Footer />
       </div>
-    </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
