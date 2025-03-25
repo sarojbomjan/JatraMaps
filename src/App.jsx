1923507f59e4d49efff7376a92b026154b698e5d
.android <sarojbomjan7@gmail.com>
2025-03-25 13:30:41 +0545

    Dashboard Initiated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom'; // Removed BrowserRouter import
 import MainLayout from './components/main_layout';
 import DashboardLayout from './components/dashboard/DashboardLayout';
+import DashboardOverview from './components/dashboard/dashboard_overview';
 
 const Home = lazy(() => import('./pages/Home'));
 const AboutUs = lazy(() => import('./pages/about'));
@@ -26,7 +27,7 @@ const App = () => {
         
         {/* Dashboard routes with its own layout */}
         <Route path="/dashboard" element={<DashboardLayout />}>
-          <Route index element={<div>Dashboard Home</div>} />
+          <Route index element={<DashboardOverview />} />
         </Route>
       </Routes>
     </Suspense>
